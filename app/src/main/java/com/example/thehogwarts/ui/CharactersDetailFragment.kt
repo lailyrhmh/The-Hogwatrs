d5818ee389bf87cbbba59d47c3f9234799ab4eb0
lailyrhmh <lailyrhmh11@gmail.com>
2022-05-26 19:38:18 +0800

    VM detail

--- app/src/main/java/com/example/thehogwarts/ui/CharactersDetailFragment.kt
+++ app/src/main/java/com/example/thehogwarts/ui/CharactersDetailFragment.kt
@@ -1,4 +1,23 @@
 package com.example.thehogwarts.ui
 
-class CharactersDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.thehogwarts.databinding.FragmentCharacterDetailBinding
+
+class CharactersDetailFragment : Fragment() {
+    private val viewModel: CharactersViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentCharacterDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
+    }
 }
