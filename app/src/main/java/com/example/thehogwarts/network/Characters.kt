0bddc1e59051f9ace635854c3025ed61e93d31a3
lailyrhmh <lailyrhmh11@gmail.com>
2022-05-26 19:57:09 +0800

    gradle, main activity

--- app/src/main/java/com/example/thehogwarts/network/Characters.kt
+++ app/src/main/java/com/example/thehogwarts/network/Characters.kt
@@ -4,6 +4,5 @@ data class Characters (
     val name: String,
     val species: String,
     val gender: String,
-    val house: String,
-//    @Json(name = "img_src") val imgSrcUrl: String
+    val house: String
 )
